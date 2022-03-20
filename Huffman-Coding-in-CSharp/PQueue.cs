82e9385ba9fa504f3ca86566355e1244038cceb4
bmc22 <90981587+bmc22@users.noreply.github.com>
2022-03-20 15:23:07 +0000

    Add files via upload

---
+++ Huffman-Coding-in-CSharp/PQueue.cs
@@ -0,0 +1,56 @@
+﻿using Huffman_Coding_in_CSharp.tree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Huffman_Coding_in_CSharp
+{
+    public class PQueue
+    {
+        private List<Node> queue;
+
+        public PQueue()
+        {
+            queue = new List<Node>();
+        }
+
+        public void enqueue(Node newNode)
+        {
+            //If the queue is empty or the node n must be inserted in the last position, add it to the queue and return
+            if (size() == 0 || queue[size() - 1].Freq <= newNode.Freq)
+            {
+                queue.Add(newNode);
+                return;
+            }
+
+            //If iteration is needed to insert the node n, we need to add a null node, which will be overwritten, to the end of the queue, to allow a space for n to be inserted
+            queue.Add(null);
+
+            //All the nodes that have a greater frequency than n will be moved by one postion to the right in order to leave a location to insert n
+            int i = size() - 1;
+            while (i > 0 && queue[i - 1].Freq > newNode.Freq)
+            {
+                queue[i]=  queue[i - 1];
+                i--;
+            }
+
+            queue[i] = newNode;
+
+        }
+        public Node dequeue()
+        {
+            if (size() == 0)
+                return null;
+            
+            var item= queue[0];
+            queue.RemoveAt(0);
+            return item;
+        }
+        public int size()
+        {
+            return queue.Count;
+        }
+    }
+}
---
+++ Huffman-Coding-in-CSharp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using Huffman_Coding_in_CSharp.tree;
+using Windows.Storage.Streams;
+
+namespace Huffman_Coding_in_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var inputPath = "etc/text.txt";
+            var outputPath = "etc/code.hc";
+
+
+            try
+            {
+                var input = File.ReadAllText(inputPath);
+                var hc = Huffman.encode(input);
+
+
+                //save the data and code into a file
+                hc.save(outputPath);
+
+                //read the HuffmanCoding object to decode the text
+                var hc2 = HuffmanCoding.read(outputPath);
+
+                //decode the text and compare it to the original input 
+                Console.WriteLine((Huffman.decode(hc2.Code, hc2.Data)).Equals(input)); //True
+
+            }
+
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+    }
+
+}
+
+     
+
